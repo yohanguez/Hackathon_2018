5e143dfe7e6dfebf8a9cfb274feecc219edd1d65
yohanguez <guez.yohan@gmail.com>
2018-01-28 19:46:25 +0200

    change name function

--- Fetching_data/program.py
+++ Fetching_data/program.py
@@ -3,7 +3,7 @@ import image_recog
 test_reco = image_recog.Image_recog(
     "/Users/carlalasry/Downloads/carla1.png",
                         "/Users/carlalasry/Downloads/arthur.jpg", "/Users/carlalasry/Desktop/carla_smile.jpg", "/Users/carlalasry/Desktop/carla_not_smile.jpg")
-print(test_reco.check_similarity())
+print(test_reco.is_similar())
 #print(test_reco.similarity)
-print(test_reco.check_is_smiling())
+print(test_reco.is_similar())
 #print(test_reco.is_smiling)
