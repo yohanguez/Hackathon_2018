beaa092eff357a15b4d90a2a2999708662dffe1c
yohanguez <guez.yohan@gmail.com>
2018-01-28 20:09:16 +0200

    change folder

--- Fetching_data/image_recog.py
+++ Fetching_data/image_recog.py
@@ -120,8 +120,18 @@ class Image_recog:
 
 
 
-test_reco = Image_recog("/Users/carlalasry/Downloads/carla1.png", "/Users/carlalasry/Downloads/arthur.jpg", "/Users/carlalasry/Desktop/carla_smile.jpg", "/Users/carlalasry/Desktop/carla_not_smile.jpg")
-print(test_reco.check_similarity())
+#test_reco = Image_recog("/Users/carlalasry/Downloads/carla1.png",
+# "/Users/carlalasry/Downloads/arthur.jpg", "/Users/carlalasry/Desktop/carla_smile.jpg", "/Users/carlalasry/Desktop/carla_not_smile.jpg")
+#print(test_reco.check_similarity())
 #print(test_reco.similarity)
-print(test_reco.check_is_smiling())
+#print(test_reco.check_is_smiling())
 #print(test_reco.is_smiling)
+
+
+
+
+            #plt.plot(x_1 + x_2, y_1 + y_2)
+            #plt.plot(x_1[0], y_1[0], "*")
+            #plt.plot(x_1[3], y_1[3], "*")
+            #plt.plot(x_2[0], y_2[0], "*")
+            #plt.savefig('/Users/carlalasry/Desktop/' + str(i))
