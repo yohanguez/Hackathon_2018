184062f18fa8247d0b09f9948c92c5161abbc2bb
yohanguez <guez.yohan@gmail.com>
2018-01-28 19:42:17 +0200

    create program

--- Fetching_data/image_recog.py
+++ Fetching_data/image_recog.py
@@ -120,9 +120,3 @@ class Image_recog:
 
 
 
-test_reco = Image_recog("/Users/carlalasry/Downloads/carla1.png", "/Users/carlalasry/Downloads/arthur.jpg", "/Users/carlalasry/Desktop/carla_smile.jpg", "/Users/carlalasry/Desktop/carla_not_smile.jpg")
-print(test_reco.check_similarity())
-#print(test_reco.similarity)
-print(test_reco.check_is_smiling())
-#print(test_reco.is_smiling)
-
