243154757387bc33649f4d5a99ab2d281f49909a
yohanguez <guez.yohan@gmail.com>
2018-01-28 19:49:03 +0200

    change folder

--- Fetching_data/program.py
+++ Fetching_data/program.py
@@ -1,8 +1,8 @@
 import image_recog
 
 test_reco = image_recog.Image_recog(
-    "/Users/carlalasry/Downloads/carla1.png",
-                        "/Users/carlalasry/Downloads/arthur.jpg", "/Users/carlalasry/Desktop/carla_smile.jpg", "/Users/carlalasry/Desktop/carla_not_smile.jpg")
+    "data/carla1.png", "data/arthur.jpg", "data/carla_smile.jpg",
+    "data/carla_not_smile.jpg")
 print(test_reco.is_similar())
 #print(test_reco.similarity)
 print(test_reco.is_similar())
