f65b2c9c38d9495c1d82ea98eab9fb552f49ea50
yohanguez <guez.yohan@gmail.com>
2018-01-29 10:04:39 +0200

    upload pictures

--- Fetching_data/camera_tagging.py
+++ Fetching_data/camera_tagging.py
@@ -14,11 +14,11 @@ import cv2
 video_capture = cv2.VideoCapture(0)
 
 # Load a sample picture and learn how to recognize it.
-obama_image = face_recognition.load_image_file("/Users/carlalasry/Downloads/carla1.png")
+obama_image = face_recognition.load_image_file("data/half_closed.jpg")
 obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
 
 # Load a second sample picture and learn how to recognize it.
-biden_image = face_recognition.load_image_file("/Users/carlalasry/Downloads/arthur.jpg")
+biden_image = face_recognition.load_image_file("data/estelle.jpg")
 biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
 
 # Create arrays of known face encodings and their names
@@ -27,8 +27,8 @@ known_face_encodings = [
     biden_face_encoding
 ]
 known_face_names = [
-    "Carla",
-    "Joe Biden"
+    "Yohan",
+    "Estelle"
 ]
 
 # Initialize some variables
