e7a76b6cb290da46d78bd8863bf55caa3873af5e
Joachim Hagege <cyberjoac@gmail.com>
2018-01-31 18:29:31 +0200

    cleanup

--- Fetching_data/program.py
+++
@@ -1,9 +0,0 @@
-from image_recognition import image_recog
-
-test_reco = image_recog.Image_recog("data/half_closed.jpg",
-                                    "data/arthur.jpg",
-                                    "data/arthur.jpg")
-print(test_reco.is_similar())
-#print(test_reco.similarity)
-print(test_reco.is_smiling_ratio())
-#print(test_reco.is_smiling)
